68a57788d20f0ae7f3e96617faec2f9e23b1a923
ayesha777 <ayeshaabadan901@gmail.com>
2019-04-21 21:29:47 +0500

    Layout2

---
+++ DB3/DB3/Controllers/ComapnyController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DB3.Controllers
+{
+    public class ComapnyController : Controller
+    {
+        // GET: Comapny
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Comapny/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Comapny/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comapny/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Comapny/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Comapny/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Comapny/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Comapny/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DB3/DB3/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DB3.Models
+{
+    public class Company
+    {
+        public string C_Name { get; set; }
+        public string Address { get; set; }
+        public string Mobile_Number { get; set; }
+
+    }
+}
