bd3dcd795446752636badb3c167a175bc61b8697
Umayya Majid <umayya4094@gmail.com>
2019-04-23 21:25:07 +0500

    mainpage

--- DB3/DB3/Models/Medicine.cs
+++ DB3/DB3/Models/Medicine.cs
@@ -7,15 +7,5 @@ namespace DB3.Models
 {
     public class Medicine
     {
-        public int Medicine_Id { get; set; }
-        public string Medicine_Name { get; set; }
-        public int Price { get; set; }
-        public DateTime Mfg_Date { get; set; }
-        public DateTime Exp_Date { get; set; }
-        public int Batch { get; set; }
-        public int Type { get; set; }
-
-        public int CompanyID { get; set; }
-
     }
 }
